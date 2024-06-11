c3a85e9b80264396f204ad13e0c47c04b8e72da7
Esther Kwashie <ekwashie001@st.ug.edu.gh>
2024-06-11 11:09:54 +0000

    Array methods in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -163,3 +163,50 @@ const alphabets = ['A', 'B', 'C', 'D'];
 alphabets;
 alphabets.join(' ');
 alphabets.join(' ').split(' ')
+
+// Write a function that will take firstname, lastname and return fullname;
+
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+};
+
+// Invoking the function
+fullName('Joe', 'Ganner');
+
+// Write a function that will take a user with firstname, lastname and return fullname
+const user = {
+  firstname: 'Joe',
+  lastname: 'Ganner'
+}
+function fullname(user) {
+  return {
+    ...user,
+    fullname: `${user.firstname} ${user.lastname}`
+  }
+}
+fullname(user);
+
+//Array map
+const users = [
+  {firstname: 'Linda', lastname: 'Parker'},
+  {firstname: 'Mark', lastname: 'Twaine'},
+  {firstname: 'Oliver', lastname: 'Queen'},
+  {firstname: 'Stephen', lastname: 'Grider'},
+  {firstname: 'Elon', lastname: 'Musk'},
+]
+users.map(fullname);
+
+// Map and return squares of numbers
+
+function square(number) {
+  return number ** 2;
+}
+
+const numbers = [0, 1, 2, 3, 4, 5]
+numbers.map(square);
+
+// Filter and return even numbers
+function even(number) {
+  return number % 2 === 0;
+}
+numbers.filter(even);
